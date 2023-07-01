012dc2757a60ac1a489a32d48fddf48001092b9d
Dewa Dwi <51353195+dewade22@users.noreply.github.com>
2023-07-01 13:09:09 +0800

    Add Framework Core

---
+++ BackEnd/Framework/TA.Framework.Core/Constant/UidTableConstant.cs
@@ -0,0 +1,9 @@
+﻿namespace TA.Framework.Core.Constant
+{
+    public class UidTableConstant
+    {
+        public const string Roles = "roles";
+        public const string UserAccount = "useraccount";
+        public const string UserInRole = "userinrole";
+    }
+}
---
+++ BackEnd/Framework/TA.Framework.Core/SystemCode/SystemCodeModel.cs
@@ -0,0 +1,33 @@
+﻿namespace TA.Framework.Core.SystemCode
+{
+    public class SystemCodeModel<T>
+    {
+        #region Constructor
+
+        public SystemCodeModel(
+            T value,
+            string description,
+            string group = "",
+            byte type = 0)
+        {
+            this.Value = value;
+            this.Description = description;
+            this.Group = group;
+            this.Type = type;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public T Value { get; private set; }
+
+        public string Description { get; private set; }
+
+        public string Group { get; private set; }
+
+        public byte Type { get; private set; }
+
+        #endregion
+    }
+}
